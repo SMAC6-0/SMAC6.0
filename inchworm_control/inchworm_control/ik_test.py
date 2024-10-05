58b69f2ca5f058d2c4e2f7e371788941640996ad
Katarzyna <kracka@wpi.edu>
2024-10-05 17:49:28 -0400

    debugging

--- inchworm_control/inchworm_control/ik_test.py
+++ inchworm_control/inchworm_control/ik_test.py
@@ -79,7 +79,7 @@ class IkTest(Node):
         try:
             pos = msg.data
             positions = pos.split(', ')
-            print("IK command receiving/splitting successful")
+            self.get_logger().info("IK command receiving/splitting successful")
             [theta1, theta2, theta3, theta4, theta5] = inverseKinematics(float(positions[0]), float(positions[1]), float(positions[2]), float(positions[3]), float(positions[4]))
             print("IK calculations successful")
             self.move_to(theta2, theta3, theta4, 10)
