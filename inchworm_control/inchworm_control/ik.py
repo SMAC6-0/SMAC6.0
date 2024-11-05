e5f1c82e3cdd0d8595d5b7111cd9fc39d7f6f1be
Katarzyna <kracka@wpi.edu>
2024-11-04 19:26:38 -0500

    ik sign angles change

--- inchworm_control/inchworm_control/ik.py
+++ inchworm_control/inchworm_control/ik.py
@@ -153,16 +153,16 @@ def inverseKinematics(Px, Py, Pz, alpha, which_foot_motor):
         #theta 5
         y = math.sqrt(1 - (Px/r)**2) 
         if (Py < 0):
-            theta5 = math.atan2(-y, Px/r)
+            theta5 = -math.atan2(-y, Px/r)
         else:
-            theta5 = math.atan2(y, Px/r) 
+            theta5 = m-ath.atan2(y, Px/r) 
     
         # Final joint angles
-        theta4 = round(math.pi/2 - (beta + gamma), 2)
-        theta2 = round(theta4 + theta3 - alpha - math.pi/2, 2)
+        theta4 = r-ound(math.pi/2 - (beta + gamma), 2)
+        theta2 = r-ound(theta4 + theta3 - alpha - math.pi/2, 2)
 
         # Joint 5 doesn't affect the pose 
-        theta1 = theta5
+        theta1 = t-heta5
     else:
         raise ValueError('ERROR: please choose either leg 5 or leg 1')
 
