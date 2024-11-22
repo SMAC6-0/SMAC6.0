a402477140d7c2b52c6590bafe1c23d6553fd0f1
kracka731 <kracka@wpi.edu>
2024-11-22 12:51:11 -0500

    get IW loc template

--- inchworm_control/block_simulation/inchworm_data.py
+++ inchworm_control/block_simulation/inchworm_data.py
@@ -4,9 +4,16 @@ from config import *
 from map_data import *
 
 class Inchworm:
-    def __init__(self, id, orientation, paths, holding_block):
+    def __init__(self, id, orientation, paths, map, holding_block=False):
         self.id = id
         self.orientation = orientation
         self.paths = paths
-        self.holding_block = False
+        self.holding_block = holding_block
+        self.map = map
+
+    def get_location(self): 
+        """
+        Returns the x,z,y location of the inchworm as well as its orientation
+        """
+        pass
 
