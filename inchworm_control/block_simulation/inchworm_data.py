2c0b08883a847fbe67bb178e63d0ce679226a9db
kracka731 <kracka@wpi.edu>
2024-12-03 11:18:47 -0500

    IW class skeleton

--- inchworm_control/block_simulation/inchworm_data.py
+++ inchworm_control/block_simulation/inchworm_data.py
@@ -4,12 +4,21 @@ from config import *
 from map_data import *
 
 class Inchworm:
-    def __init__(self, id, orientation, paths, map, holding_block=False):
+    def __init__(self, id: int, orientation, paths: list[Cell], map: list[int], location: list[int], holding_block=False):
+        """
+        Initialize one inchworm (abbreviated as IW) in the system.
+        Args: 
+            id (int): This inchworm's ID number. Used to set paths in the map. 
+            orientation (Enum): the direction that the IW's leading leg is facing, relative to the world grid's frame. 
+            paths(list[Cell]): The Cells through which this inchworm will travel. (May be multiple, ie to the supply depot then to the structure.)
+            map (list): xzy (3D) list storing the current status of the map, as this inchworm knows it. 
+        """
         self.id = id
         self.orientation = orientation
         self.paths = paths
-        self.holding_block = holding_block
         self.map = map
+        self.location = location
+        self.holding_block = holding_block
 
     def get_location(self): 
         """
@@ -17,3 +26,17 @@ class Inchworm:
         """
         pass
 
+    def clear_my_path(self): 
+        pass
+
+    def plan_path(self, end): 
+        pass
+
+    # TODO: insert state machine here 
+
+# def __main__():
+#     # TODO: KASIA IS TESTING, DELETE THIS LATER 
+
+#     inchwormA = Inchworm(1, InchwormOrientation.NORTH, )
+#     print(" got to the end of main mon ami! ")
+#     pass
