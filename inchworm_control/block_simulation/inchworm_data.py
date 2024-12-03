9c9fc85cef47f507cdd16759866315c8b3291a84
kracka731 <kracka@wpi.edu>
2024-12-03 11:28:08 -0500

    comments

--- inchworm_control/block_simulation/inchworm_data.py
+++ inchworm_control/block_simulation/inchworm_data.py
@@ -7,11 +7,13 @@ class Inchworm:
     def __init__(self, id: int, orientation, paths: list[Cell], map: list[int], location: list[int], holding_block=False):
         """
         Initialize one inchworm (abbreviated as IW) in the system.
-        Args: 
+        Args:
             id (int): This inchworm's ID number. Used to set paths in the map. 
             orientation (Enum): the direction that the IW's leading leg is facing, relative to the world grid's frame. 
             paths(list[Cell]): The Cells through which this inchworm will travel. (May be multiple, ie to the supply depot then to the structure.)
-            map (list): xzy (3D) list storing the current status of the map, as this inchworm knows it. 
+            map (list[int]): xzy (3D) list storing the current status of the map, as this inchworm knows it. 
+            location (list[int]): the xzy location of the inchworm's leading foot. 
+            holding_block (bool): True if the inchworm's leading foot is holding a block. 
         """
         self.id = id
         self.orientation = orientation
@@ -37,6 +39,8 @@ class Inchworm:
 # def __main__():
 #     # TODO: KASIA IS TESTING, DELETE THIS LATER 
 
-#     inchwormA = Inchworm(1, InchwormOrientation.NORTH, )
+#     inchwormA = Inchworm(1, InchwormOrientation.NORTH, [], [[[]]], [1,1,1])
+
+#     inchwormA.holding_block
 #     print(" got to the end of main mon ami! ")
 #     pass
