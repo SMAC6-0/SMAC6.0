648d724836f3529782d2b3ce241445b50f200450
SakshiGauro <gaurosakshi113@gmail.com>
2024-12-06 13:41:16 -0500

    setup of state machine

--- inchworm_control/inchworm_control/state_machine.py
+++ inchworm_control/inchworm_control/state_machine.py
@@ -3,6 +3,24 @@ from rclpy.node import Node
 from std_msgs.msg import Float32, String
 
 from enum import Enum
+from transitions import Machine
 
-class Inchworm_State(Enum):
-    INITIALIZATION = 1
+# Defining the Inchworm states 
+Inchworm_States = ["INITIALIZATION", "PATH_PLANNING", "TRAVELLING_TO_SUPPLY", "TRANSPORTING_BLOCK", "PLACING_BLOCK", "ERROR", "STRUCTURE_COMPLETE"]
+
+# class for Finite State Machine
+class Inchworm_StateMachine:
+    def __init__(self):
+        # initial state
+        self.machine = Machine(model=self, states=Inchworm_States, initial= "INITIALIZATION")
+
+        # add transitions
+        self.machine.add_transition(trigger="map_snapshot", source="INITIALIZATION")
+
+
+
+    
+
+
+
+
