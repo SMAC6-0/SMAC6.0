09323880e85ca23ecf4880101642c4bd5cccc782
tracy <tracy3846@gmail.com>
2025-01-28 12:34:45 -0500

    made it work!

--- inchworm_control/setup.py
+++ inchworm_control/setup.py
@@ -1,20 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='inchworm_project',  # Overall project name
-    version='0.0.1',
-    packages=find_packages(exclude=['tests']),  # Finds all sub-packages
-    install_requires=['setuptools'],  # Dependencies
-
-    # Optional metadata
-    zip_safe=True,
-    maintainer='your_name',
-    maintainer_email='your_email@example.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
-
-    # Entry points for any scripts within your packages
+    name='inchworm_project',
+    version='0.1.0',
+    packages=find_packages(),  # Automatically find all packages
+    install_requires=[],  # Add dependencies here if needed
     entry_points={
         'console_scripts': [
             'motor_controller = inchworm_control.motor_controller:main',
