8730cc86498e74892b2f766cc1ff81ea08e9badd
SakshiGauro <gaurosakshi113@gmail.com>
2025-02-04 10:49:29 -0500

    debugging

--- inchworm_control/inchworm_control/simple_iw_sm.py
+++ inchworm_control/inchworm_control/simple_iw_sm.py
@@ -137,7 +137,9 @@ class Inchworm:
         # append msg_len, block_change, checksum, ending_code(enum) to buffer
 
         buffer.append(msg_len)
-        # + checksum + block_change + UART_CODES.Initialization
+        buffer.append(checksum)
+        buffer.append(block_change)
+        buffer.append(UART_CODES.Initialization)
 
         print("buffer before bytearray", buffer)
 
