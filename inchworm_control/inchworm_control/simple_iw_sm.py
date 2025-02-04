10a71b93386d1de0a7f2fe78e8ca9e9a24617667
SakshiGauro <gaurosakshi113@gmail.com>
2025-02-04 11:01:40 -0500

    debugging

--- inchworm_control/inchworm_control/simple_iw_sm.py
+++ inchworm_control/inchworm_control/simple_iw_sm.py
@@ -134,7 +134,7 @@ class Inchworm:
 
         # append msg_len, block_change, checksum, ending_code(enum) to buffer
 
-        buffer += msg_len + checksum + block_change + UART_CODES.Initialization
+        buffer += msg_len + checksum + block_change + struct.pack('B', UART_CODES.Initialization)
 
         iw_serial.write(buffer)
 
