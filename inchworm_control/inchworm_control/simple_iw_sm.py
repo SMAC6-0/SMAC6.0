19ac8d94c0aca14237383e5993d0816ef7ccb996
SakshiGauro <gaurosakshi113@gmail.com>
2025-02-12 06:55:09 -0500

    debug

--- inchworm_control/inchworm_control/simple_iw_sm.py
+++ inchworm_control/inchworm_control/simple_iw_sm.py
@@ -32,7 +32,7 @@ IW_identifier = 1 # this is the idenifier that goes infornt of the message to be
 IW_message_counter = 0 # this is the messgae counter for sending data, IK's message counter increases
 
 # in X, Y, Z format to be consistent with Mo's code
-IW_PATH = [[4, 1, 0], [5, 1, 0], [6, 1, 0], [7, 1, 0], [7, 2, 0], [7, 3, 0], [7, 4, 0], [7, 5, 0], [7, 6, 0], [7, 7, 0], [7, 8, 1]] 
+IW_PATH = [[1, 1, 0], [2, 1, 0], [3, 1, 0]] 
 
 # final_map = [
 #     [ # X = 0
