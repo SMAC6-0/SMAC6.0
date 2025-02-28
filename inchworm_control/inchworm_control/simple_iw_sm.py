4b42b09403703bdb56fd87d29ae5fe4984cd5644
SakshiGauro <gaurosakshi113@gmail.com>
2025-02-28 11:51:58 -0500

    debug

--- inchworm_control/inchworm_control/simple_iw_sm.py
+++ inchworm_control/inchworm_control/simple_iw_sm.py
@@ -735,7 +735,7 @@ class Inchworm:
         # checksum.append(buffer.pop())
         # checksum.append(buffer.pop())
         checksum = bytearray(checksum)
-        checksum = int.from_bytes(bytes(checksum), 'little')
+        checksum = int.from_bytes(bytes(checksum), 'big')
         # checksum = int.from_bytes(checksum,'big')
         return checksum
 
