2e5ff2f2e951d01b97eb4b1db69c289f0368f4a8
SakshiGauro <gaurosakshi113@gmail.com>
2025-02-28 11:39:17 -0500

    debug

--- inchworm_control/inchworm_control/simple_iw_sm.py
+++ inchworm_control/inchworm_control/simple_iw_sm.py
@@ -729,7 +729,8 @@ class Inchworm:
         # msgLen = int.from_bytes(bytes(msgLenBytes), 'little')
         # checksum =  int.from_bytes(checksum, 'big')  # Convert to integer
         checksum =[]
-        checksum = buffer[-2:]
+        checksum += buffer[-1:]
+        checksum += buffer[-2:]
         print("checksum: ", checksum)
 
         # checksum.append(buffer.pop())
