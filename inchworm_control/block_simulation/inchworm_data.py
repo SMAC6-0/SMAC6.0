abfb6d63ccf76e54aadf3a8f914d3ac2f409d38f
SakshiGauro <gaurosakshi113@gmail.com>
2025-03-04 12:42:32 -0500

    testing gitignore

--- inchworm_control/block_simulation/inchworm_data.py
+++ inchworm_control/block_simulation/inchworm_data.py
@@ -740,6 +740,8 @@ if __name__ == "__main__":
     with open("/home/smac/robot_ws/src/SMAC6.0/Final_Structure.txt", "r") as file:
       final_structure = file.readlines()
 
+      
+
     inchworm = Inchworm(orientation=CURRENT_ORIENTATION, final_structure=final_structure, location=IW_1_LOC, holding_block=False)
     try:
         inchworm.run()
