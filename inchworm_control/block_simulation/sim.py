fc57bdf7cd58c456db767e731db239f84526c6f9
Sakshi Gauro <59516534+SakshiGauro@users.noreply.github.com>
2025-03-03 19:04:13 -0500

    Update sim.py

--- inchworm_control/block_simulation/sim.py
+++ inchworm_control/block_simulation/sim.py
@@ -323,8 +323,8 @@ class Voxel(Button):
                     print("Block not found")
                 destroy(self)
                 
-        # if key == "escape":
-        #     stop_simulation()
+        if key == "escape":
+            stop_simulation()
 
 # Skybox
 class Sky(Entity):
