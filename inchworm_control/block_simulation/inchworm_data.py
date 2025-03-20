4a1904ddef2de311d2047130aaf58aa0995dee42
SakshiGauro <gaurosakshi113@gmail.com>
2025-03-20 11:59:26 -0400

    fixed the final map array

--- inchworm_control/block_simulation/inchworm_data.py
+++ inchworm_control/block_simulation/inchworm_data.py
@@ -755,8 +755,6 @@ if __name__ == "__main__":
     with open("/home/smac/robot_ws/src/SMAC6.0/Final_Structure.txt", "r") as file:
       final_structure = file.readlines()
 
-      
-    print("I'm hereee")
     inchworm = Inchworm(orientation=IW_ORIENTATIONS[0], final_structure=final_structure, location=IW_LOCS[0], holding_block=False)
     try:
         inchworm.run()
