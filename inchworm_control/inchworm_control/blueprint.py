5987c62a2ffad97ed02d24aba09f3248ffec4898
SakshiGauro <gaurosakshi113@gmail.com>
2025-03-20 12:15:01 -0400

    debug

--- inchworm_control/inchworm_control/blueprint.py
+++ inchworm_control/inchworm_control/blueprint.py
@@ -32,10 +32,10 @@ def blueprint(curr_map, final_map) -> list:
     # final_map = np.array(final_map)
     # arrays are not the same size
     print("Curr Map size", curr_map.shape)
-    print("final Map size", final_map.shape)
+    print("final Map size", final_map.size)
     print("Final Map", final_map)
     print("Curr Map", curr_map)
-    if curr_map.shape != final_map.shape:
+    if curr_map.shape != final_map.size:
         print("Arrays don't match shape") 
         return [-9,-9,-9] # error value
 
