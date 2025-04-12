92829171525a13bd268803442bc29ad1b4e4e788
Rickwjx <rickwjx85@gmail.com>
2025-04-12 14:29:23 -0400

    id write, testing new motor

--- inchworm_control/inchworm_control/motor_reading.py
+++ inchworm_control/inchworm_control/motor_reading.py
@@ -3,7 +3,9 @@ from lewansoul_servo_bus import ServoBus
 
 servo_bus = ServoBus('/dev/ttyUSB0')
 
-motor_id = input("What motor's position would you like to read?")
+servo_bus.id_write(1, 3)
 
-print(servo_bus.pos_read(int(motor_id)))
+# motor_id = input("What motor's position would you like to read?")
+
+# print(servo_bus.pos_read(int(motor_id)))
 # print(servo_bus.id_read())
