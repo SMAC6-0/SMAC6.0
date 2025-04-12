79385337fbc13c563d97b9a0bf2e804f6a407bbb
Rickwjx <rickwjx85@gmail.com>
2025-04-11 21:43:13 -0400

    test

--- inchworm_control/inchworm_control/motor_controller.py
+++ inchworm_control/inchworm_control/motor_controller.py
@@ -89,7 +89,7 @@ class MotorController(Node):
         activate_servo(self.servo1)
         activate_servo(self.servo2)
         release_servo(self.servo1)
-        # release_servo_newEE(self.servo2)
+        release_servo_newEE(self.servo2)
 
     def listener_callback(self, msg):
         """
