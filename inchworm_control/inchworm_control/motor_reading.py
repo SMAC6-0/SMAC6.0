31d18e07f3c12693eea1de9a9bdd2f79b8aa1d1c
Rickwjx <rickwjx85@gmail.com>
2025-04-17 19:53:43 -0400

    continuous read motor pos

--- inchworm_control/inchworm_control/motor_reading.py
+++ inchworm_control/inchworm_control/motor_reading.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from lewansoul_servo_bus import ServoBus
+from time import sleep
 
 servo_bus = ServoBus('/dev/ttyUSB0')
 
@@ -7,5 +8,7 @@ servo_bus = ServoBus('/dev/ttyUSB0')
 
 motor_id = input("What motor's position would you like to read?")
 
-print(servo_bus.pos_read(int(motor_id)))
+while(True):
+    print(servo_bus.pos_read(int(motor_id)))
+    sleep(0.5)
 # print(servo_bus.id_read())
