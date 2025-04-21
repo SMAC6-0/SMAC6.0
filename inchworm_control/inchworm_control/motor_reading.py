d8713a3861541a36ac6290b2179eec6453775701
Rickwjx <rickwjx85@gmail.com>
2025-04-21 16:19:25 -0400

    Updated motor reading and reverted to tuned IK

--- inchworm_control/inchworm_control/motor_reading.py
+++ inchworm_control/inchworm_control/motor_reading.py
@@ -6,20 +6,24 @@ servo_bus = ServoBus('/dev/ttyUSB0')
 
 # servo_bus.id_write(4, 2)
 
-# motor_id = input("What motor's position would you like to read?")
+mode = input("Would you like to read (1) or move (2)?")
 
-# print(servo_bus.mode_read(int(motor_id)))
+if int(mode) == 1:
+    motor_id = input("What motor's position would you like to read?")
 
-# while(True):
-#     print(servo_bus.pos_read(int(motor_id)))
-#     sleep(0.5)
+    # print(servo_bus.mode_read(int(motor_id)))
 
-motor_pos = input("What position would you like to move the motor to?")
+    while(True):
+        print(servo_bus.pos_read(int(motor_id)))
+        sleep(0.5)
 
-currTime = time()
-servo_bus.move_time_write(5, int(motor_pos), 1)
-while(time() - currTime < 1.0):
-    print(servo_bus.pos_read(5))
-    sleep(0.1)
+elif int(mode) == 2:
+    motor_pos = input("What position would you like to move the motor to?")
+
+    currTime = time()
+    servo_bus.move_time_write(5, int(motor_pos), 1)
+    while(time() - currTime < 1.0):
+        print(servo_bus.pos_read(5))
+        sleep(0.1)
 
 # print(servo_bus.id_read())
