1d1e95eca89413e02e2daf00c2c4610fd5597a48
Rickwjx <rickwjx85@gmail.com>
2025-04-20 22:23:43 -0400

    x change

--- inchworm_control/inchworm_control/ik.py
+++ inchworm_control/inchworm_control/ik.py
@@ -79,7 +79,7 @@ def adjust_positions(goal_X: float, goal_Y: float, goal_Z: float, pivot_foot: in
         #     inputY = CUBE_WIDTH * 1.0101 * goal_Y - 0.0204
         #     inputZ = (-0.0233 * (CUBE_WIDTH * goal_Z)**2) + (CUBE_WIDTH * 1.2747 * goal_Z) + 0.4137
         if (pivot_foot == 1):
-            inputX = CUBE_WIDTH * goal_X + 1
+            inputX = CUBE_WIDTH * goal_X + 1.2
             inputY = CUBE_WIDTH * goal_Y
             inputZ = CUBE_WIDTH * goal_Z + 1.6
         elif (pivot_foot == 5):
