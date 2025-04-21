14119bdefe533642054aeabca39fd8de81c50400
Rickwjx <rickwjx85@gmail.com>
2025-04-20 20:27:07 -0400

    Moving IW so it doesn't catch on itself first

--- inchworm_control/inchworm_control/ik_test.py
+++ inchworm_control/inchworm_control/ik_test.py
@@ -264,14 +264,16 @@ class IkTest(Node):
         # above_goal = copy.deepcopy(goal)
         # above_goal[2] += 0.5
 
-        z_test = [1, 0, 1, EE_direction.DOWN.value]
+        current_pos = [2, 0, 0, EE_direction.DOWN.value]
+
+        z_test = [2, 0, 1, EE_direction.DOWN.value]
 
         # Start moving leading foot 
         pivot_foot = 1 
-        self.latch_detach(pivot_foot) 
+        self.latch_detach(pivot_foot)
         
         # EE moves straight up from board to just above the home
-        self.move_to(HOME_POSITION, z_test, BLOCK_INTERFACING_TIME, pivot_foot) 
+        self.move_to(current_pos, z_test, BLOCK_INTERFACING_TIME, pivot_foot) 
 
         # # Move forward and hover over the goal position 
         # self.move_to(ABOVE_HOME, above_goal, TRAVEL_TIME, pivot_foot)
