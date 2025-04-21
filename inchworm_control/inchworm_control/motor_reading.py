f5c08e6eb0e80ab675e330e5da4623e74e5dfec7
Rickwjx <rickwjx85@gmail.com>
2025-04-21 16:07:20 -0400

    Read motor pos

--- inchworm_control/inchworm_control/motor_reading.py
+++ inchworm_control/inchworm_control/motor_reading.py
@@ -6,20 +6,20 @@ servo_bus = ServoBus('/dev/ttyUSB0')
 
 # servo_bus.id_write(4, 2)
 
-# motor_id = input("What motor's position would you like to read?")
+motor_id = input("What motor's position would you like to read?")
 
 # print(servo_bus.mode_read(int(motor_id)))
 
-# while(True):
-#     print(servo_bus.pos_read(int(motor_id)))
-#     sleep(0.5)
+while(True):
+    print(servo_bus.pos_read(int(motor_id)))
+    sleep(0.5)
 
-motor_pos = input("What position would you like to move the motor to?")
+# motor_pos = input("What position would you like to move the motor to?")
 
-currTime = time()
-servo_bus.move_time_write(5, int(motor_pos), 1)
-while(time() - currTime < 1.0):
-    print(servo_bus.pos_read(5))
-    sleep(0.1)
+# currTime = time()
+# servo_bus.move_time_write(5, int(motor_pos), 1)
+# while(time() - currTime < 1.0):
+#     print(servo_bus.pos_read(5))
+#     sleep(0.1)
 
 # print(servo_bus.id_read())
