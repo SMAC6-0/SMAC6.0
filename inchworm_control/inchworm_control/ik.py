9abcaf03146bad6f0dbc1269f7d60b6167ae0c0a
Rickwjx <rickwjx85@gmail.com>
2025-04-21 19:07:06 -0400

    Changing tuning again

--- inchworm_control/inchworm_control/ik.py
+++ inchworm_control/inchworm_control/ik.py
@@ -71,8 +71,8 @@ def adjust_positions(goal_X: float, goal_Y: float, goal_Z: float, pivot_foot: in
             list: 3x1 list 
         """
         if (pivot_foot == 1):
-            inputX = CUBE_WIDTH * 1.144 * goal_X + 0.1242
-            inputY = CUBE_WIDTH * 1.1595 * goal_Y + 0.0249
+            inputX = CUBE_WIDTH * 1.244 * goal_X + 0.1242
+            inputY = CUBE_WIDTH * 1.2595 * goal_Y + 0.0249
             inputZ = CUBE_WIDTH * 1.0786 * goal_Z - 0.0432
         elif (pivot_foot == 5):
             inputX = CUBE_WIDTH * 1.0158 * goal_X - 0.0059
