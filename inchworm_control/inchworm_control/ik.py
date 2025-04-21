72079ca7b657c97fddc19571b20f2f9965a0f6b2
Rickwjx <rickwjx85@gmail.com>
2025-04-21 18:43:17 -0400

    Removed alpha offset

--- inchworm_control/inchworm_control/ik.py
+++ inchworm_control/inchworm_control/ik.py
@@ -121,7 +121,7 @@ def inverseKinematics(Px, Py, Pz, alpha, pivot_foot: int, fix_EE_orientation: bo
 
     
     # Convert alpha angle from degrees to radians 
-    alpha = math.radians(alpha - 10)
+    alpha = math.radians(alpha)
 
     # error catching: Robot cannot reach the point
     maxDistArmZ = L_BASE + L1 + L2 + L3 + L4 + L_ENDEFFECTOR # max Distance the arm can reach in z axis
