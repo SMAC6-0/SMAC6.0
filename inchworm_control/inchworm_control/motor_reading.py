815f120585671e6acbf8ff143f29e132852e7d66
Rickwjx <rickwjx85@gmail.com>
2025-04-21 15:34:22 -0400

    time?

--- inchworm_control/inchworm_control/motor_reading.py
+++ inchworm_control/inchworm_control/motor_reading.py
@@ -16,9 +16,9 @@ servo_bus = ServoBus('/dev/ttyUSB0')
 
 motor_pos = input("What position would you like to move the motor to?")
 
-currTime = time
+currTime = time()
 servo_bus.move_time_write(5, int(motor_pos), 1)
-while(time - currTime < 1.0):
+while(time() - currTime < 1.0):
     print(servo_bus.pos_read(5))
 
 # print(servo_bus.id_read())
