6ce021d4d3413f6e018e23880fc52257dfc43503
SakshiGauro <gaurosakshi113@gmail.com>
2025-04-22 18:41:36 -0400

    fixing imports

--- inchworm_control/setup.py
+++ inchworm_control/setup.py
@@ -10,7 +10,7 @@ setup(
             'motor_controller = inchworm_control.motor_controller:main',
             'step_publisher = inchworm_control.step_publisher:main',
             'execute_path = inchworm_control.execute_path:main',
-            'inchworm_data = block_simulation.inchworm_data:main',
+            'inchworm_data = inchworm_control.block_simulation.inchworm_data:main',
         ],
     },
 )
