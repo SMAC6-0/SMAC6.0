3e49da98e5d5956a0bf0a0853709568c4283224d
SakshiGauro <gaurosakshi113@gmail.com>
2025-04-22 18:35:30 -0400

    correctly importing?

--- inchworm_control/block_simulation/inchworm_data.py
+++ inchworm_control/block_simulation/inchworm_data.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from enum import Enum
 import copy
-from config import *
+from inchworm_control.config import *
 import map_data
 import blueprint as bp
 from time import sleep
