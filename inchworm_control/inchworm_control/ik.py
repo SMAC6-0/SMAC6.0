9bb86b769ba1ca22b2db2aedd4b5222ad3139b4a
SakshiGauro <gaurosakshi113@gmail.com>
2025-04-22 14:42:00 -0400

    neaggtive

--- inchworm_control/inchworm_control/ik.py
+++ inchworm_control/inchworm_control/ik.py
@@ -169,9 +169,9 @@ def inverseKinematics(Px, Py, Pz, alpha, pivot_foot: int, fix_EE_orientation: bo
         else: 
             if Py > 0: # Y value is positive aka turning left
                 print("Im hereeeee Turning left")
-                theta5 = theta1 + 45
-            elif (Py < 0): 
                 theta5 = theta1 - 45
+            elif (Py < 0): 
+                theta5 = theta1 + 45
             else: 
                 theta5 = theta1
 
