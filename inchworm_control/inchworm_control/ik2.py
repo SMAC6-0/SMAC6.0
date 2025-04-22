4a173612a858cac9c3e98ea163ffbce4f95bbbf0
Rickwjx <rickwjx85@gmail.com>
2025-04-21 23:12:07 -0400

    Adding cases for each step since IK needs tuning and discrete is easier than general equation

--- inchworm_control/inchworm_control/ik2.py
+++ inchworm_control/inchworm_control/ik2.py
@@ -71,7 +71,14 @@ def adjust_positions(goal_X: float, goal_Y: float, goal_Z: float, pivot_foot: in
             list: 3x1 list 
         """
         if (pivot_foot == 1):
-            inputX = CUBE_WIDTH * 0.98 * goal_X + 1.2
+            # Here we go!
+            if goal_X <= 1:
+                inputX = CUBE_WIDTH * goal_X + 2
+            elif goal_X <= 2:
+                inputX = CUBE_WIDTH * 0.98 * goal_X + 1.2
+            else:
+                inputX = CUBE_WIDTH * 0.98 * goal_X + 1.2
+            
             inputY = CUBE_WIDTH * 1.2595 * goal_Y + 0.0249
             inputZ = CUBE_WIDTH * 1.0786 * goal_Z - 0.0432
         elif (pivot_foot == 5):
