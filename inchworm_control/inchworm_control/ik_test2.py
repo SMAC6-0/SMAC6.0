32f41ae33361af427a4e8f5a6ee3bb50d7b7660c
Rickwjx <rickwjx85@gmail.com>
2025-04-23 00:58:01 -0400

    Starting to test turns (y != 0)

--- inchworm_control/inchworm_control/ik_test2.py
+++ inchworm_control/inchworm_control/ik_test2.py
@@ -537,22 +537,27 @@ class IkTest2(Node):
 
         # Pivot Foot 1 x = 2
         # Gradual
-        self.move_to(x2z0, x2z0_5, TRAVEL_TIME, pivot_foot)
+        # self.move_to(x2z0, x2z0_5, TRAVEL_TIME, pivot_foot)
 
-        self.move_to(x2z0_5, x2z1, TRAVEL_TIME, pivot_foot)
+        # self.move_to(x2z0_5, x2z1, TRAVEL_TIME, pivot_foot)
 
-        self.move_to(x2z1, x2z1_5, TRAVEL_TIME, pivot_foot)
+        # self.move_to(x2z1, x2z1_5, TRAVEL_TIME, pivot_foot)
 
-        self.move_to(x2z1_5, x2z2, TRAVEL_TIME, pivot_foot)
+        # self.move_to(x2z1_5, x2z2, TRAVEL_TIME, pivot_foot)
 
-        self.move_to(x2z2, x2z2_5, TRAVEL_TIME, pivot_foot)
+        # self.move_to(x2z2, x2z2_5, TRAVEL_TIME, pivot_foot)
 
-        self.move_to(x2z2_5, x2z3, TRAVEL_TIME, pivot_foot)
+        # self.move_to(x2z2_5, x2z3, TRAVEL_TIME, pivot_foot)
 
-        self.move_to(x2z3, x2z3_5, TRAVEL_TIME, pivot_foot)
+        # self.move_to(x2z3, x2z3_5, TRAVEL_TIME, pivot_foot)
 
         # Direct
         # self.move_to(x2z0, x2z2_5, TRAVEL_TIME, pivot_foot)
+
+
+        # Pivot Foot 1 y = -1
+        # Gradual
+        self.move_to(x1z0, y_1z0, TRAVEL_TIME, pivot_foot)
         
 
         print("Movement complete: TEST_STEP")
