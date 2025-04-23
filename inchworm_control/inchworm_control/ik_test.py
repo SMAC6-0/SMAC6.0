918721d062cc0528b0c9ff4b4cc7fd9d7751868f
SakshiGauro <gaurosakshi113@gmail.com>
2025-04-23 14:18:12 -0400

    placing block z = 3

--- inchworm_control/inchworm_control/ik_test.py
+++ inchworm_control/inchworm_control/ik_test.py
@@ -583,9 +583,9 @@ class IkTest(Node):
         # self.move_to(x2z2_5, x2z2, TRAVEL_TIME, pivot_foot)
 
         # place block z = 2
-        # self.move_to(x2z2_5, x2z3, TRAVEL_TIME, pivot_foot)
+        self.move_to(x2z2_5, x2z3, TRAVEL_TIME, pivot_foot)
         self.move_to(x2z3, x2z3_5, TRAVEL_TIME, pivot_foot)
-        self.move_to(x2z3_5, x2z3, TRAVEL_TIME, pivot_foot)
+        # self.move_to(x2z3_5, x2z3, TRAVEL_TIME, pivot_foot)
         
 
         print("Movement complete: TEST_STEP")
