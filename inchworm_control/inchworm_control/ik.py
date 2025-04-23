4ad0cc2d840f76905d35c5f4e2610d548ad36fc6
SakshiGauro <gaurosakshi113@gmail.com>
2025-04-23 10:51:31 -0400

    x1z1

--- inchworm_control/inchworm_control/ik.py
+++ inchworm_control/inchworm_control/ik.py
@@ -87,7 +87,7 @@ def adjust_positions(goal_X: float, goal_Y: float, goal_Z: float, alpha: float, 
                         adjustedAlpha = True
                     elif goal_Z > 0.75 and goal_Z <= 1.25: # z = 1
                         inputX = CUBE_WIDTH * goal_X + 1.0
-                        inputZ = CUBE_WIDTH * goal_Z + 0.6
+                        inputZ = CUBE_WIDTH * goal_Z + 0.3
                         inputAlpha = alpha - 2
                         adjustedAlpha = True
                     elif goal_Z > 1.25 and goal_Z <= 2: # z = 1.5
