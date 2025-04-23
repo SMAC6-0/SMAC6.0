5b686ef0cc96f15ad0c99f8471e75db3649e8738
SakshiGauro <gaurosakshi113@gmail.com>
2025-04-23 11:26:44 -0400

    x2z1

--- inchworm_control/inchworm_control/ik_test.py
+++ inchworm_control/inchworm_control/ik_test.py
@@ -552,7 +552,7 @@ class IkTest(Node):
         # Pivot Foot 1 x = 1
         # Gradual
         # self.move_to(x1z0, x1z0, TRAVEL_TIME, pivot_foot)
-        self.move_to(x1z0, x1z0_5, TRAVEL_TIME, pivot_foot)
+        # self.move_to(x1z0, x1z0_5, TRAVEL_TIME, pivot_foot)
         # self.move_to(x1z0_5, x1z1, TRAVEL_TIME, pivot_foot)
 
         # self.move_to(x1z1, x1z1_5, TRAVEL_TIME, pivot_foot)
@@ -571,9 +571,9 @@ class IkTest(Node):
 
         # Pivot Foot 1 x = 2
         # Gradual
-        self.move_to(x1z0_5, x2z0_5, TRAVEL_TIME, pivot_foot)
         self.move_to(x2z0_5, x2z0, TRAVEL_TIME, pivot_foot)
-        # self.move_to(x2z0, x2z0_5, TRAVEL_TIME, pivot_foot)
+        self.move_to(x1z0_5, x2z0_5, TRAVEL_TIME, pivot_foot)
+        self.move_to(x2z0_5, x2z1, TRAVEL_TIME, pivot_foot)
         
 
         print("Movement complete: TEST_STEP")
