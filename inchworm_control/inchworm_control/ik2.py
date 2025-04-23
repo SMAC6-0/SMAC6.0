056f1b13300428d4d11b491d752cfdea92bb16a8
Rickwjx <rickwjx85@gmail.com>
2025-04-23 15:38:02 -0400

    Added tilt when moving block

--- inchworm_control/inchworm_control/ik2.py
+++ inchworm_control/inchworm_control/ik2.py
@@ -118,10 +118,15 @@ def adjust_positions(goal_X: float, goal_Y: float, goal_Z: float, alpha: float, 
                         inputZ = CUBE_WIDTH * goal_Z + 0.4
                         inputAlpha = alpha - 8
                         adjustedAlpha = True
-                    elif goal_Z > 0.75 and goal_Z <= 1.25: # z = 1
+                    elif goal_Z > 0.75 and goal_Z <= 1.05: # z = 1
                         inputX = CUBE_WIDTH * goal_X + 1
                         inputZ = CUBE_WIDTH * goal_Z + 0.58
                         inputAlpha = alpha - 8
+                        adjustedAlpha = True
+                    elif goal_Z > 1.05 and goal_Z <= 1.25: # z = 1.25 (Tilt When moving block)
+                        inputX = CUBE_WIDTH * goal_X + 1
+                        inputZ = CUBE_WIDTH * goal_Z + 0.58
+                        inputAlpha = alpha + 10
                         adjustedAlpha = True
                     elif goal_Z > 1.25 and goal_Z <= 1.75: # z = 1.5
                         inputX = CUBE_WIDTH * goal_X + 1.2
@@ -169,11 +174,17 @@ def adjust_positions(goal_X: float, goal_Y: float, goal_Z: float, alpha: float, 
                     inputZ = CUBE_WIDTH * goal_Z + 0.2
                     inputAlpha = alpha - 5
                     adjustedAlpha = True
-                elif goal_Z > 0.75 and goal_Z <= 1.25: # z = 1
+                elif goal_Z > 0.75 and goal_Z <= 1.05: # z = 1
                     inputX = CUBE_WIDTH * goal_X + 0.8
                     inputY = CUBE_WIDTH * goal_Y - 0.8
                     inputZ = CUBE_WIDTH * goal_Z + 0.5
                     inputAlpha = alpha - 8
+                    adjustedAlpha = True
+                elif goal_Z > 1.05 and goal_Z <= 1.25: # z = 1.25 (Tilt when moving block)
+                    inputX = CUBE_WIDTH * goal_X + 0.8
+                    inputY = CUBE_WIDTH * goal_Y - 0.8
+                    inputZ = CUBE_WIDTH * goal_Z + 0.5
+                    inputAlpha = alpha + 10
                     adjustedAlpha = True
                 elif goal_Z > 1.25 and goal_Z <= 1.75: # z = 1.5
                     inputX = CUBE_WIDTH * goal_X + 0.8
--- inchworm_control/inchworm_control/ik_test2.py
+++ inchworm_control/inchworm_control/ik_test2.py
@@ -609,9 +609,9 @@ class IkTest2(Node):
         # self.move_to(x2z1_5, x2z1, TRAVEL_TIME, pivot_foot)
 
         # Step Forward With Block
-        # self.move_to(x1z1, x1z1_5, TRAVEL_TIME, pivot_foot)
-        # self.move_to(x1z1_5, x2z1_5, TRAVEL_TIME, pivot_foot)
-        # self.move_to(x2z1_5, x2z1, TRAVEL_TIME, pivot_foot)
+        self.move_to(x1z1, x1z1_5, TRAVEL_TIME, pivot_foot)
+        self.move_to(x1z1_5, x2z1_5, TRAVEL_TIME, pivot_foot)
+        self.move_to(x2z1_5, x2z1, TRAVEL_TIME, pivot_foot)
 
 
         # Turn Right
@@ -620,9 +620,9 @@ class IkTest2(Node):
         # self.move_to(y_1z0_5, y_1z0, TRAVEL_TIME, pivot_foot)
 
         # Turn Right With Block
-        self.move_to(x1z1, x1z1_5, TRAVEL_TIME, pivot_foot)
-        self.move_to(x1z1_5, y_1z1_5, TRAVEL_TIME, pivot_foot)
-        self.move_to(y_1z1_5, y_1z1, TRAVEL_TIME, pivot_foot)
+        # self.move_to(x1z1, x1z1_5, TRAVEL_TIME, pivot_foot)
+        # self.move_to(x1z1_5, y_1z1_5, TRAVEL_TIME, pivot_foot)
+        # self.move_to(y_1z1_5, y_1z1, TRAVEL_TIME, pivot_foot)
 
 
         print("Movement complete: TEST_STEP")
