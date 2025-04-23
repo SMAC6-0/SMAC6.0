91ae5a02ca27fd3a549964fb64e3b749a2cba1d1
Rickwjx <rickwjx85@gmail.com>
2025-04-22 20:06:38 -0400

    Alpha change

--- inchworm_control/inchworm_control/ik2.py
+++ inchworm_control/inchworm_control/ik2.py
@@ -83,7 +83,7 @@ def adjust_positions(goal_X: float, goal_Y: float, goal_Z: float, alpha: float, 
                     elif goal_Z > 0.01 and goal_Z <= 0.75: # z = 0.5
                         inputX = CUBE_WIDTH * goal_X + 1.2
                         inputZ = CUBE_WIDTH * goal_Z + 0.3
-                        inputAlpha = alpha + 10
+                        inputAlpha = alpha - 8
                     elif goal_Z > 0.75 and goal_Z <= 1.25: # z = 1
                         inputX = CUBE_WIDTH * goal_X + 0.4
                         inputZ = CUBE_WIDTH * goal_Z + 0
