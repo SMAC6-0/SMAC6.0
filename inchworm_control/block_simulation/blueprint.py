541ba8c6e87bfc405b1017422a2ae13c0d7531a7
mominhnguyen <mo.minhnguyen@gmail.com>
2025-04-22 22:34:06 -0400

    debugging

--- inchworm_control/block_simulation/blueprint.py
+++ inchworm_control/block_simulation/blueprint.py
@@ -46,6 +46,8 @@ def blueprint(curr_map, final_map, repeat_threshold=2):
 
     if curr_map.shape != final_map.shape:
         print("Arrays don't match shape") 
+        print("curr_map shape: ", curr_map.shape)
+        print("final_map shape: ", final_map.shape)
         return [-9,-9,-9], build_queue # error value
 
     elif curr_map.size == final_map.size:
