ea84654ee2490fa1d760fa86ea0d374c0dec351a
Rickwjx <rickwjx85@gmail.com>
2025-04-23 15:16:09 -0400

    Turn right with block

--- inchworm_control/inchworm_control/ik_test2.py
+++ inchworm_control/inchworm_control/ik_test2.py
@@ -590,11 +590,11 @@ class IkTest2(Node):
         # Gradual
         # self.move_to(x1z0, y_1z0, TRAVEL_TIME, pivot_foot)
 
-        self.move_to(y_1z0, y_1z0_5, TRAVEL_TIME, pivot_foot)
+        # self.move_to(y_1z0, y_1z0_5, TRAVEL_TIME, pivot_foot)
 
-        self.move_to(y_1z0_5, y_1z1, TRAVEL_TIME, pivot_foot)
+        # self.move_to(y_1z0_5, y_1z1, TRAVEL_TIME, pivot_foot)
 
-        self.move_to(y_1z1, y_1z1_5, TRAVEL_TIME, pivot_foot)
+        # self.move_to(y_1z1, y_1z1_5, TRAVEL_TIME, pivot_foot)
         
 
         # Movements Pivot Foot 1
@@ -618,6 +618,11 @@ class IkTest2(Node):
         # self.move_to(x1z0, x1z0_5, TRAVEL_TIME, pivot_foot)
         # self.move_to(x1z0_5, y_1z0_5, TRAVEL_TIME, pivot_foot)
         # self.move_to(y_1z0_5, y_1z0, TRAVEL_TIME, pivot_foot)
+
+        # Turn Right With Block
+        self.move_to(x1z1, x1z1_5, TRAVEL_TIME, pivot_foot)
+        self.move_to(x1z1_5, y_1z1_5, TRAVEL_TIME, pivot_foot)
+        self.move_to(y_1z1_5, y_1z1, TRAVEL_TIME, pivot_foot)
 
 
         print("Movement complete: TEST_STEP")
