3b7fc2ca3b9b6bde63e17ac48edc44038bc52713
kracka731 <kracka@wpi.edu>
2025-04-22 20:02:40 -0400

    specify import folder

--- inchworm_control/block_simulation/inchworm_data.py
+++ inchworm_control/block_simulation/inchworm_data.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 from enum import Enum
 import copy
-from config import *
-import map_data
-import blueprint as bp
+from block_simulation.config import *
+import block_simulation.map_data as map_data
+import block_simulation.blueprint as bp
 from time import sleep
 import serial
 import struct
