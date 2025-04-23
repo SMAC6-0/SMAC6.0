1d20471f2efec2219a6c729071278fca85dccec3
Rickwjx <rickwjx85@gmail.com>
2025-04-23 00:00:34 -0400

    Testing x2z1.5

--- inchworm_control/inchworm_control/ik_test2.py
+++ inchworm_control/inchworm_control/ik_test2.py
@@ -538,8 +538,10 @@ class IkTest2(Node):
 
         self.move_to(x2z0_5, x2z1, TRAVEL_TIME, pivot_foot)
 
+        self.move_to(x2z1, x2z1_5, TRAVEL_TIME, pivot_foot)
+
         # Direct
-        self.move_to(x2z0, x2z1, TRAVEL_TIME, pivot_foot)
+        # self.move_to(x2z0, x2z1, TRAVEL_TIME, pivot_foot)
         
 
         print("Movement complete: TEST_STEP")
