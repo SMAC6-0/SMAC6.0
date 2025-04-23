34bdcce5057cbeb4348a0d395d8dc1bbf8d57fac
Rickwjx <rickwjx85@gmail.com>
2025-04-22 22:30:33 -0400

    Shifting x back for a few

--- inchworm_control/inchworm_control/ik2.py
+++ inchworm_control/inchworm_control/ik2.py
@@ -81,22 +81,22 @@ def adjust_positions(goal_X: float, goal_Y: float, goal_Z: float, alpha: float, 
                         inputX = CUBE_WIDTH * goal_X + 1
                         inputZ = CUBE_WIDTH * goal_Z - 0.3
                     elif goal_Z > 0.01 and goal_Z <= 0.75: # z = 0.5
-                        inputX = CUBE_WIDTH * goal_X + 1.8
+                        inputX = CUBE_WIDTH * goal_X + 1.6
                         inputZ = CUBE_WIDTH * goal_Z + 0.3
                         inputAlpha = alpha - 8
                         adjustedAlpha = True
                     elif goal_Z > 0.75 and goal_Z <= 1.25: # z = 1
-                        inputX = CUBE_WIDTH * goal_X + 1.6
+                        inputX = CUBE_WIDTH * goal_X + 1.3
                         inputZ = CUBE_WIDTH * goal_Z + 0.6
                         inputAlpha = alpha - 8
                         adjustedAlpha = True
                     elif goal_Z > 1.25 and goal_Z <= 2: # z = 1.5
-                        inputX = CUBE_WIDTH * goal_X + 1.4
+                        inputX = CUBE_WIDTH * goal_X + 1.0
                         inputZ = CUBE_WIDTH * goal_Z + 0.6
                         inputAlpha = alpha - 8
                         adjustedAlpha = True
                     elif goal_Z > 2 and goal_Z <= 3: # z = 2.5
-                        inputX = CUBE_WIDTH * goal_X + 1.2
+                        inputX = CUBE_WIDTH * goal_X + 0.8
                         inputZ = CUBE_WIDTH * goal_Z + 0.6
                         inputAlpha = alpha -8
                         adjustedAlpha = True
