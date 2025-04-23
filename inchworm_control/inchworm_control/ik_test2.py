46fdc1f4a9e95f286e99a88edf6444552f76832a
Rickwjx <rickwjx85@gmail.com>
2025-04-23 18:56:18 -0400

    Turn right

--- inchworm_control/inchworm_control/ik_test2.py
+++ inchworm_control/inchworm_control/ik_test2.py
@@ -629,15 +629,15 @@ class IkTest2(Node):
 
 
         # Turn Right
-        # self.move_to(x1z0, x1z0_5, TRAVEL_TIME, pivot_foot)
-        # self.move_to(x1z0_5, y_1z0_5, TRAVEL_TIME, pivot_foot)
-        # self.move_to(y_1z0_5, y_1z0, TRAVEL_TIME, pivot_foot)
+        self.move_to(x1z0, x1z0_5, TRAVEL_TIME, pivot_foot)
+        self.move_to(x1z0_5, y_1z0_5, TRAVEL_TIME, pivot_foot)
+        self.move_to(y_1z0_5, y_1z0, TRAVEL_TIME, pivot_foot)
 
         # Turn Right With Block
-        activate_servo(self.servo2)
-        self.move_to(x1z1, x1z1_5, TRAVEL_TIME, pivot_foot)
-        self.move_to(x1z1_5, y_1z1_5, TRAVEL_TIME, pivot_foot)
-        self.move_to(y_1z1_5, y_1z1, TRAVEL_TIME, pivot_foot)
+        # activate_servo(self.servo2)
+        # self.move_to(x1z1, x1z1_5, TRAVEL_TIME, pivot_foot)
+        # self.move_to(x1z1_5, y_1z1_5, TRAVEL_TIME, pivot_foot)
+        # self.move_to(y_1z1_5, y_1z1, TRAVEL_TIME, pivot_foot)
 
 
         print("Movement complete: TEST_STEP")
