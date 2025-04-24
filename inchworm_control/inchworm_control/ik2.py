bdd1fe712ae5c74837b0a48f433954ff3e97b4b5
Rickwjx <rickwjx85@gmail.com>
2025-04-23 23:53:51 -0400

    Flip it back since turning away from block pulls inchworm back

--- inchworm_control/inchworm_control/ik2.py
+++ inchworm_control/inchworm_control/ik2.py
@@ -210,7 +210,7 @@ def adjust_positions(goal_X: float, goal_Y: float, goal_Z: float, alpha: float, 
                     elif goal_Z > -0.85 and goal_Z <= -0.25: # z = -0.5
                         inputX = CUBE_WIDTH * goal_X + 1
                         inputZ = CUBE_WIDTH * goal_Z + 0.2
-                        inputAlpha = alpha - 10
+                        inputAlpha = alpha + 10
                     elif goal_Z > -0.25 and goal_Z <= 0.25: # z = 0
                         inputX = CUBE_WIDTH * goal_X + 0.9
                         inputZ = CUBE_WIDTH * goal_Z + 0.05
@@ -225,11 +225,11 @@ def adjust_positions(goal_X: float, goal_Y: float, goal_Z: float, alpha: float, 
                     if goal_Z > -1.1 and goal_Z <= -0.75: # z = -1
                         inputX = CUBE_WIDTH * goal_X + 1
                         inputZ = CUBE_WIDTH * goal_Z + 0.5
-                        inputAlpha = alpha - 20
+                        inputAlpha = alpha + 20
                     elif goal_Z > -0.75 and goal_Z <= -0.25: # z = -0.5
                         inputX = CUBE_WIDTH * goal_X + 1
-                        inputZ = CUBE_WIDTH * goal_Z + 0.2
-                        inputAlpha = alpha - 20
+                        inputZ = CUBE_WIDTH * goal_Z + 0.3
+                        inputAlpha = alpha + 20
                     elif goal_Z > -0.25 and goal_Z <= 0.35: # z = 0
                         inputX = CUBE_WIDTH * goal_X + 0.5
                         inputZ = CUBE_WIDTH * goal_Z + 0.2
