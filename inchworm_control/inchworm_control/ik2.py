bdfaecb9553b1a23bfdabdd1793f626821cdfdea
Rickwjx <rickwjx85@gmail.com>
2025-04-24 01:07:54 -0400

    Testing pf1 y-1 values in pf5

--- inchworm_control/inchworm_control/ik2.py
+++ inchworm_control/inchworm_control/ik2.py
@@ -246,31 +246,31 @@ def adjust_positions(goal_X: float, goal_Y: float, goal_Z: float, alpha: float, 
                 inputY = CUBE_WIDTH * goal_Y + 0
 
             # Here are the turns
-            elif goal_Y <= -0.5: # y = -1
+            elif goal_Y <= -0.5: # y = -1 Finish Turn Left
                 if goal_Z > -1.1 and goal_Z <= -0.75: # z = -1
-                    inputX = CUBE_WIDTH * goal_X + 0
-                    inputY = CUBE_WIDTH * goal_Y + 0
-                    inputZ = CUBE_WIDTH * goal_Z + 0
-                    inputAlpha = alpha - 0
+                    inputX = CUBE_WIDTH * goal_X + 0.6
+                    inputY = CUBE_WIDTH * goal_Y - 0.6
+                    inputZ = CUBE_WIDTH * goal_Z - 0.3
+                    inputAlpha = alpha - 5
                 elif goal_Z > -0.75 and goal_Z <= -0.25: # z = -0.5
-                    inputX = CUBE_WIDTH * goal_X + 0
-                    inputY = CUBE_WIDTH * goal_Y + 0
-                    inputZ = CUBE_WIDTH * goal_Z + 0
-                    inputAlpha = alpha - 0
+                    inputX = CUBE_WIDTH * goal_X + 0.7
+                    inputY = CUBE_WIDTH * goal_Y - 1
+                    inputZ = CUBE_WIDTH * goal_Z + 0.2
+                    inputAlpha = alpha - 5
                 elif goal_Z > -0.25 and goal_Z <= 0.25: # z = 0
-                    inputX = CUBE_WIDTH * goal_X + 0
-                    inputY = CUBE_WIDTH * goal_Y + 0
-                    inputZ = CUBE_WIDTH * goal_Z + 0
-                    inputAlpha = alpha - 0
+                    inputX = CUBE_WIDTH * goal_X + 0.7
+                    inputY = CUBE_WIDTH * goal_Y - 0.9
+                    inputZ = CUBE_WIDTH * goal_Z + 0.5
+                    inputAlpha = alpha - 8
                 elif goal_Z > 0.25 and goal_Z <= 0.75: # z = 0.5
-                    inputX = CUBE_WIDTH * goal_X + 0
-                    inputY = CUBE_WIDTH * goal_Y + 0
-                    inputZ = CUBE_WIDTH * goal_Z + 0
-                    inputAlpha = alpha - 0
+                    inputX = CUBE_WIDTH * goal_X + 0.8
+                    inputY = CUBE_WIDTH * goal_Y - 0.8
+                    inputZ = CUBE_WIDTH * goal_Z + 0.5
+                    inputAlpha = alpha - 8
                 else:
                     print(f"Position not included Z: {goal_Z}")
             
-            elif goal_Y > 0.5: # y = 1
+            elif goal_Y > 0.5: # y = 1 Finish Turn Right
                 if goal_Z > -1.1 and goal_Z <= -0.75: # z = -1
                     inputX = CUBE_WIDTH * goal_X + 0
                     inputY = CUBE_WIDTH * goal_Y + 0
--- inchworm_control/inchworm_control/ik_test2.py
+++ inchworm_control/inchworm_control/ik_test2.py
@@ -587,8 +587,8 @@ class IkTest2(Node):
         
 
         # Movements Pivot Foot 1
-        pivot_foot = 1
-        self.latch_detach(pivot_foot)
+        # pivot_foot = 1
+        # self.latch_detach(pivot_foot)
         # Step Forward
         # self.move_to(x1z0, x1z0_5, TRAVEL_TIME, pivot_foot)
         # self.move_to(x1z0_5, x2z0_5, TRAVEL_TIME, pivot_foot)
@@ -632,9 +632,9 @@ class IkTest2(Node):
 
 
         # Turn Left
-        self.move_to(x1z0, x1z0_5, TRAVEL_TIME, pivot_foot)
-        self.move_to(x1z0_5, y1z0_5, TRAVEL_TIME, pivot_foot)
-        self.move_to(y1z0_5, y1z0, TRAVEL_TIME, pivot_foot)
+        # self.move_to(x1z0, x1z0_5, TRAVEL_TIME, pivot_foot)
+        # self.move_to(x1z0_5, y1z0_5, TRAVEL_TIME, pivot_foot)
+        # self.move_to(y1z0_5, y1z0, TRAVEL_TIME, pivot_foot)
 
         # Turn Left With Block
         # activate_servo(self.servo2)
@@ -644,8 +644,8 @@ class IkTest2(Node):
 
 
         # Movements Pivot Foot 5
-        # pivot_foot = 5
-        # self.latch_detach(pivot_foot)
+        pivot_foot = 5
+        self.latch_detach(pivot_foot)
 
         # Step Forward (Run like step forward first and then reverse to be normal step forward)
         # self.move_to(x1z0, x1z0_5, TRAVEL_TIME, pivot_foot)
@@ -662,6 +662,12 @@ class IkTest2(Node):
         # self.move_to(x2z_1, x2z_075, TRAVEL_TIME, pivot_foot)
         # self.move_to(x2z_075, x1z_075, TRAVEL_TIME, pivot_foot)
         # self.move_to(x1z_075, x1z_1, TRAVEL_TIME, pivot_foot)
+
+        # Turn Left
+        self.move_to(y_1z0, y_1z0_5, TRAVEL_TIME, pivot_foot)
+        # self.move_to(y_1z0_5, x1z0_5, TRAVEL_TIME, pivot_foot)
+        # self.move_to(x1z0_5, x1z0, TRAVEL_TIME, pivot_foot)
+
 
         # Turn Right
 
