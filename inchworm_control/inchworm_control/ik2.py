abf004c9ad51201257dca6c272594cf61209a643
Rickwjx <rickwjx85@gmail.com>
2025-04-23 20:50:09 -0400

    Actually x1z0

--- inchworm_control/inchworm_control/ik2.py
+++ inchworm_control/inchworm_control/ik2.py
@@ -204,22 +204,21 @@ def adjust_positions(goal_X: float, goal_Y: float, goal_Z: float, alpha: float, 
             if goal_Y > -0.5 and goal_Y <= 0.5: # y = 0
                 if goal_X > 0.9 and goal_X <= 1.5: # x = 1
                     if goal_Z > -1.1 and goal_Z <= -0.75: # z = -1
-                        print("HEREEEEEEEEEEEEEEEEEEEEE")
-                        inputX = CUBE_WIDTH * goal_X + 1.5
-                        inputZ = CUBE_WIDTH * goal_Z + 0
-                        inputAlpha = alpha + 8
-                    elif goal_Z > -0.75 and goal_Z <= -0.25: # z = -0.5
                         inputX = CUBE_WIDTH * goal_X + 1
                         inputZ = CUBE_WIDTH * goal_Z + 0
-                        inputAlpha = alpha + 8
+                        inputAlpha = alpha - 8
+                    elif goal_Z > -0.75 and goal_Z <= -0.25: # z = -0.5
+                        inputX = CUBE_WIDTH * goal_X + 1.8
+                        inputZ = CUBE_WIDTH * goal_Z + 0
+                        inputAlpha = alpha - 8
                     elif goal_Z > -0.25 and goal_Z <= 0.25: # z = 0
-                        inputX = CUBE_WIDTH * goal_X + 0
+                        inputX = CUBE_WIDTH * goal_X + 1
                         inputZ = CUBE_WIDTH * goal_Z + 0
-                        inputAlpha = alpha - 0
+                        inputAlpha = alpha - 5
                     elif goal_Z > 0.25 and goal_Z <= 0.75: # z = 0.5
-                        inputX = CUBE_WIDTH * goal_X + 0
+                        inputX = CUBE_WIDTH * goal_X + 1
                         inputZ = CUBE_WIDTH * goal_Z + 0
-                        inputAlpha = alpha - 0
+                        inputAlpha = alpha - 5
                     else:
                         print(f"Position not included Z: {goal_Z}")
                 elif goal_X > 1.5 and goal_X <= 2.1: # x = 2
