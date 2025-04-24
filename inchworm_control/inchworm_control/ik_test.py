b10014d401b7b2f3c00424fc85a49eb2061f282d
SakshiGauro <gaurosakshi113@gmail.com>
2025-04-24 15:43:16 -0400

    left home

--- inchworm_control/inchworm_control/ik_test.py
+++ inchworm_control/inchworm_control/ik_test.py
@@ -590,9 +590,9 @@ class IkTest(Node):
         
         # Pivot Foot 1 x = 1
         # Turns
-        self.move_to(x1z0, x1z0_5, TRAVEL_TIME, pivot_foot)
-        self.move_to(x1z0_5, y_1z0_5, TRAVEL_TIME, pivot_foot)
-        self.move_to(y_1z0_5, y_1z0, TRAVEL_TIME, pivot_foot)
+        # self.move_to(x1z0, x1z0_5, TRAVEL_TIME, pivot_foot)
+        # self.move_to(x1z0_5, y_1z0_5, TRAVEL_TIME, pivot_foot)
+        # self.move_to(y_1z0_5, y_1z0, TRAVEL_TIME, pivot_foot)
         # self.move_to(y_1z0, y_1z0_5, TRAVEL_TIME, pivot_foot)
         # self.move_to(y_1z0_5, y_1z1, TRAVEL_TIME, pivot_foot)
         # self.move_to(y_1z1, y_1z1_5, TRAVEL_TIME, pivot_foot)
@@ -606,9 +606,9 @@ class IkTest(Node):
         # self.move_to(y_1z1_5, y_1z1, TRAVEL_TIME, pivot_foot)
 
         # Left turns
-        # self.move_to(x1z0, x1z0_5, TRAVEL_TIME, pivot_foot)
-        # self.move_to(x1z0_5, y1z0_5, TRAVEL_TIME, pivot_foot)
-        # self.move_to(y1z0_5, y1z0, TRAVEL_TIME, pivot_foot)
+        self.move_to(x1z0, x1z0_5, TRAVEL_TIME, pivot_foot)
+        self.move_to(x1z0_5, y1z0_5, TRAVEL_TIME, pivot_foot)
+        self.move_to(y1z0_5, y1z0, TRAVEL_TIME, pivot_foot)
 
         # turn left w/ block
         # self.move_to(x1z1, x1z1_5, TRAVEL_TIME, pivot_foot)
