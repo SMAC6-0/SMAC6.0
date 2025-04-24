bef566c54393753e2bba9e699bacdba17fea458d
Rickwjx <rickwjx85@gmail.com>
2025-04-24 01:32:27 -0400

    Doing turning right

--- inchworm_control/inchworm_control/ik2.py
+++ inchworm_control/inchworm_control/ik2.py
@@ -272,25 +272,25 @@ def adjust_positions(goal_X: float, goal_Y: float, goal_Z: float, alpha: float, 
             
             elif goal_Y > 0.5: # y = 1 Finish Turn Right
                 if goal_Z > -1.1 and goal_Z <= -0.75: # z = -1
-                    inputX = CUBE_WIDTH * goal_X + 0
-                    inputY = CUBE_WIDTH * goal_Y + 0
-                    inputZ = CUBE_WIDTH * goal_Z + 0
-                    inputAlpha = alpha - 0
+                    inputX = CUBE_WIDTH * goal_X + 1
+                    inputY = CUBE_WIDTH * goal_Y + 0.5
+                    inputZ = CUBE_WIDTH * goal_Z - 0.1
+                    inputAlpha = alpha - 8
                 elif goal_Z > -0.75 and goal_Z <= -0.25: # z = -0.5
-                    inputX = CUBE_WIDTH * goal_X + 0
-                    inputY = CUBE_WIDTH * goal_Y + 0
-                    inputZ = CUBE_WIDTH * goal_Z + 0
-                    inputAlpha = alpha - 0
+                    inputX = CUBE_WIDTH * goal_X + 1
+                    inputY = CUBE_WIDTH * goal_Y + 1.1
+                    inputZ = CUBE_WIDTH * goal_Z + 0.2
+                    inputAlpha = alpha - 8
                 elif goal_Z > -0.25 and goal_Z <= 0.25: # z = 0
-                    inputX = CUBE_WIDTH * goal_X + 0
-                    inputY = CUBE_WIDTH * goal_Y + 0
-                    inputZ = CUBE_WIDTH * goal_Z + 0
-                    inputAlpha = alpha - 0
+                    inputX = CUBE_WIDTH * goal_X + 1
+                    inputY = CUBE_WIDTH * goal_Y + 1.0
+                    inputZ = CUBE_WIDTH * goal_Z + 0.2
+                    inputAlpha = alpha - 8
                 elif goal_Z > 0.25 and goal_Z <= 0.75: # z = 0.5
-                    inputX = CUBE_WIDTH * goal_X + 0
-                    inputY = CUBE_WIDTH * goal_Y + 0
-                    inputZ = CUBE_WIDTH * goal_Z + 0
-                    inputAlpha = alpha - 0
+                    inputX = CUBE_WIDTH * goal_X + 1
+                    inputY = CUBE_WIDTH * goal_Y + 1.0
+                    inputZ = CUBE_WIDTH * goal_Z + 0.2
+                    inputAlpha = alpha - 8
                 else:
                     print(f"Position not included Z: {goal_Z}")
             
