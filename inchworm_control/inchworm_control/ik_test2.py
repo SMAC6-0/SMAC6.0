73dcc685aacf198e1d6236163c4fd3b86363d7ab
Rickwjx <rickwjx85@gmail.com>
2025-04-24 02:52:37 -0400

    Turn left block

--- inchworm_control/inchworm_control/ik_test2.py
+++ inchworm_control/inchworm_control/ik_test2.py
@@ -714,17 +714,29 @@ class IkTest2(Node):
         # self.move_to(y_1z0_5, x1z0_5, TRAVEL_TIME, pivot_foot)
         # self.move_to(x1z0_5, x1z0, TRAVEL_TIME, pivot_foot)
 
-        # Turn Right
+        # Turn Left Block
         pivot_foot = 1
         self.latch_detach(pivot_foot)
-        self.move_to(x1z0, x1z0_5, TRAVEL_TIME, pivot_foot)
-        self.move_to(x1z0_5, y_1z0_5, TRAVEL_TIME, pivot_foot)
-        self.move_to(y_1z0_5, y_1z0, TRAVEL_TIME, pivot_foot)
+        self.move_to(x1z1, x1z1_5, TRAVEL_TIME, pivot_foot)
+        self.move_to(x1z1_5, y1z1_5, TRAVEL_TIME, pivot_foot)
+        self.move_to(y1z1_5, y1z0, TRAVEL_TIME, pivot_foot)
         pivot_foot = 5
         self.latch_detach(pivot_foot)
-        self.move_to(y1z0, y1z0_5, TRAVEL_TIME, pivot_foot)
-        self.move_to(y1z0_5, x1z0_5, TRAVEL_TIME, pivot_foot)
-        self.move_to(x1z0_5, x1z0, TRAVEL_TIME, pivot_foot)
+        self.move_to(y_1z_1, y_1z_0_5, TRAVEL_TIME, pivot_foot)
+        self.move_to(y_1z_0_5, x1z_0_5, TRAVEL_TIME, pivot_foot)
+        self.move_to(x1z_0_5, x1z_1, TRAVEL_TIME, pivot_foot)
+
+        # Turn Right
+        # pivot_foot = 1
+        # self.latch_detach(pivot_foot)
+        # self.move_to(x1z0, x1z0_5, TRAVEL_TIME, pivot_foot)
+        # self.move_to(x1z0_5, y_1z0_5, TRAVEL_TIME, pivot_foot)
+        # self.move_to(y_1z0_5, y_1z0, TRAVEL_TIME, pivot_foot)
+        # pivot_foot = 5
+        # self.latch_detach(pivot_foot)
+        # self.move_to(y1z0, y1z0_5, TRAVEL_TIME, pivot_foot)
+        # self.move_to(y1z0_5, x1z0_5, TRAVEL_TIME, pivot_foot)
+        # self.move_to(x1z0_5, x1z0, TRAVEL_TIME, pivot_foot)
 
 
         print("Movement complete: TEST_STEP")
