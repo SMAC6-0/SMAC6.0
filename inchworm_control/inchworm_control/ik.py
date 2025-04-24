d6c6b84a5661627e179a1e92a46e5683dfe034d8
SakshiGauro <gaurosakshi113@gmail.com>
2025-04-24 15:13:15 -0400

    pivot foot 5 up

--- inchworm_control/inchworm_control/ik.py
+++ inchworm_control/inchworm_control/ik.py
@@ -241,8 +241,8 @@ def adjust_positions(goal_X: float, goal_Y: float, goal_Z: float, alpha: float, 
                         inputX = CUBE_WIDTH * goal_X + 0.5
                         inputZ = CUBE_WIDTH * goal_Z + 0.5
                     elif goal_Z > 0.25 and goal_Z <= 0.75: # z = 0.5
-                        inputX = CUBE_WIDTH * goal_X + 0
-                        inputZ = CUBE_WIDTH * goal_Z + 0
+                        inputX = CUBE_WIDTH * goal_X + 0.5
+                        inputZ = CUBE_WIDTH * goal_Z + 0.5
                     else:
                         print(f"Position not included Z: {goal_Z}")
                 else:
