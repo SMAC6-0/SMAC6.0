0c58c4815b1617ceec0e3c3ed77bf0969b8c496d
Rickwjx <rickwjx85@gmail.com>
2025-04-24 13:50:01 -0400

    longer lift

--- inchworm_control/inchworm_control/ik_test2.py
+++ inchworm_control/inchworm_control/ik_test2.py
@@ -752,7 +752,7 @@ class IkTest2(Node):
         self.latch_detach(pivot_foot)
         self.move_to(y_1z_1, y_1z_075, TRAVEL_TIME + 1, pivot_foot)
         activate_servo(self.servo1)
-        self.move_to(y_1z_075, y_1z_0_5, TRAVEL_TIME + 1, pivot_foot)
+        self.move_to(y_1z_075, y_1z_0_5, TRAVEL_TIME + 2, pivot_foot)
         release_servo(self.servo1)
         self.move_to_noTP(y_1z_0_5, x1z_075, TRAVEL_TIME, pivot_foot)
         # self.move_to(y0z_0_5, y0z_1, TRAVEL_TIME, pivot_foot)
