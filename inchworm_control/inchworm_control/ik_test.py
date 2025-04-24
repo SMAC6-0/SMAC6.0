b519733147ec6631a81c098597da39bd8cc7feee
SakshiGauro <gaurosakshi113@gmail.com>
2025-04-24 16:08:33 -0400

    left home

--- inchworm_control/inchworm_control/ik_test.py
+++ inchworm_control/inchworm_control/ik_test.py
@@ -628,11 +628,10 @@ class IkTest(Node):
 
         # left 
 
-        # self.move_to(y1z0, y1z0_5, TRAVEL_TIME, pivot_foot)
-        # self.move_to(y1z0_5, x1z0_5, TRAVEL_TIME, pivot_foot)
+        self.move_to(y_1z0, y_1z0_5, TRAVEL_TIME, pivot_foot)
+        self.move_to(y_1z0_5, x1z0_5, TRAVEL_TIME, pivot_foot)
         # self.move_to(y1z0_5, x1z0_5, TRAVEL_TIME, pivot_foot)
 
-        self.move_to(x1z0, y1z0, TRAVEL_TIME, pivot_foot)
 
 
 
