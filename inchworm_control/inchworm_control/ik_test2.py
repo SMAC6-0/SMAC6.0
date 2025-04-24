e7259fe95dcdcf483aa06766d7cb75db257544a7
Rickwjx <rickwjx85@gmail.com>
2025-04-24 14:03:37 -0400

    Turning back to x1

--- inchworm_control/inchworm_control/ik_test2.py
+++ inchworm_control/inchworm_control/ik_test2.py
@@ -526,7 +526,8 @@ class IkTest2(Node):
         # Pivot Foot 5
         # x = 1
         x1z_1   = [1, 0, -1, EE_direction.DOWN.value]
-        x1z_075   = [1, 0, -0.75, EE_direction.DOWN.value]
+        x1z_075 = [1, 0, -0.75, EE_direction.DOWN.value]
+        x1z_0_6 = [1, 0, -0.6, EE_direction.DOWN.value]
         x1z_0_5 = [1, 0, -0.5, EE_direction.DOWN.value]
         #x1z0 (Already defined)
         #x1z0_5
@@ -557,7 +558,7 @@ class IkTest2(Node):
         # y = -1
         y_1z_1   = [1, -1, -1, EE_direction.DOWN.value]
         y_1z_075 = [1, -1, -0.75, EE_direction.DOWN.value]
-        y_1z_06 = [1, -1, -0.6, EE_direction.DOWN.value]
+        y_1z_06  = [1, -1, -0.6, EE_direction.DOWN.value]
         y_1z_0_5 = [1, -1, -0.5, EE_direction.DOWN.value]
         y0z_0_5  = [1, 0.000000005, -0.5, EE_direction.DOWN.value]
         y0z_1    = [1, 0.000000005, -1, EE_direction.DOWN.value]
@@ -755,7 +756,7 @@ class IkTest2(Node):
         activate_servo(self.servo1)
         self.move_to(y_1z_075, y_1z_06, TRAVEL_TIME + 2, pivot_foot)
         release_servo(self.servo1)
-        self.move_to_noTP(y_1z_0_5, y0z_0_5, TRAVEL_TIME + 4, pivot_foot)
+        self.move_to_noTP(y_1z_06, x1z_0_6, TRAVEL_TIME + 4, pivot_foot)
         # self.move_to(y0z_0_5, y0z_1, TRAVEL_TIME, pivot_foot)
 
         # Turn Right
