eceda4343695292d855f0ce0c1817ab12933ded1
Rickwjx <rickwjx85@gmail.com>
2025-04-24 13:58:52 -0400

    Slower turn

--- inchworm_control/inchworm_control/ik_test2.py
+++ inchworm_control/inchworm_control/ik_test2.py
@@ -755,7 +755,7 @@ class IkTest2(Node):
         activate_servo(self.servo1)
         self.move_to(y_1z_075, y_1z_07, TRAVEL_TIME + 2, pivot_foot)
         release_servo(self.servo1)
-        self.move_to_noTP(y_1z_0_5, y0z_0_5, TRAVEL_TIME, pivot_foot)
+        self.move_to_noTP(y_1z_0_5, y0z_0_5, TRAVEL_TIME + 4, pivot_foot)
         # self.move_to(y0z_0_5, y0z_1, TRAVEL_TIME, pivot_foot)
 
         # Turn Right
