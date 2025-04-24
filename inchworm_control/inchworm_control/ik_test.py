c53361b12ea8cefee7a9bb8a9685d1f5bd8aa8c9
SakshiGauro <gaurosakshi113@gmail.com>
2025-04-24 18:58:56 -0400

    forward block

--- inchworm_control/inchworm_control/ik_test.py
+++ inchworm_control/inchworm_control/ik_test.py
@@ -636,9 +636,9 @@ class IkTest(Node):
         # self.move_to(x1z0_5, x1z0, TRAVEL_TIME, pivot_foot)
 
         # move forward w/ blocks 
-        self.move_to(x2z1, x2z1_5, TRAVEL_TIME, pivot_foot)
-        self.move_to(x2z1_5, x1z1_5, TRAVEL_TIME, pivot_foot)
-        self.move_to(x1z1_5, x1z1, TRAVEL_TIME, pivot_foot)
+        self.move_to(x2z1, x2z_1, TRAVEL_TIME, pivot_foot)
+        self.move_to(x2z_1, x1z_0_5, TRAVEL_TIME, pivot_foot)
+        self.move_to(x1z_0_5, x1z1, TRAVEL_TIME, pivot_foot)
 
 
 
