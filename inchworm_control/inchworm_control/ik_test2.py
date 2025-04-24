b3e67c529cc2dff70898c93d34350af571474cb4
Rickwjx <rickwjx85@gmail.com>
2025-04-24 00:50:14 -0400

    Turn left

--- inchworm_control/inchworm_control/ik_test2.py
+++ inchworm_control/inchworm_control/ik_test2.py
@@ -587,6 +587,7 @@ class IkTest2(Node):
         
 
         # Movements Pivot Foot 1
+        pivot_foot = 1
         # Step Forward
         # self.move_to(x1z0, x1z0_5, TRAVEL_TIME, pivot_foot)
         # self.move_to(x1z0_5, x2z0_5, TRAVEL_TIME, pivot_foot)
@@ -630,9 +631,9 @@ class IkTest2(Node):
 
 
         # Turn Left
-        # self.move_to(x1z0, x1z0_5, TRAVEL_TIME, pivot_foot)
-        # self.move_to(x1z0_5, y1z0_5, TRAVEL_TIME, pivot_foot)
-        # self.move_to(y1z0_5, y1z0, TRAVEL_TIME, pivot_foot)
+        self.move_to(x1z0, x1z0_5, TRAVEL_TIME, pivot_foot)
+        self.move_to(x1z0_5, y1z0_5, TRAVEL_TIME, pivot_foot)
+        self.move_to(y1z0_5, y1z0, TRAVEL_TIME, pivot_foot)
 
         # Turn Left With Block
         # activate_servo(self.servo2)
@@ -640,6 +641,8 @@ class IkTest2(Node):
         # self.move_to(x1z1_5, y1z1_5, TRAVEL_TIME, pivot_foot)
         # self.move_to(y1z1_5, y1z1, TRAVEL_TIME, pivot_foot)
 
+
+        # Movements Pivot Foot 5
         # pivot_foot = 5
         # self.latch_detach(pivot_foot)
 
@@ -659,6 +662,8 @@ class IkTest2(Node):
         # self.move_to(x2z_075, x1z_075, TRAVEL_TIME, pivot_foot)
         # self.move_to(x1z_075, x1z_1, TRAVEL_TIME, pivot_foot)
 
+        # Turn Right
+
 
         # FULL MOVEMENTS
         # Step Forward
@@ -674,17 +679,17 @@ class IkTest2(Node):
         # self.move_to(x1z0_5, x1z0, TRAVEL_TIME, pivot_foot)
 
         # Step Forward Block
-        pivot_foot = 1
-        self.latch_detach(pivot_foot)
-        activate_servo(self.servo2)
-        self.move_to(x1z1, x1z1_5, TRAVEL_TIME, pivot_foot)
-        self.move_to(x1z1_5, x2z1_5, TRAVEL_TIME, pivot_foot)
-        self.move_to(x2z1_5, x2z1, TRAVEL_TIME, pivot_foot)
-        pivot_foot = 5
-        self.latch_detach(pivot_foot)
-        self.move_to(x2z_1, x2z_075, TRAVEL_TIME, pivot_foot)
-        self.move_to(x2z_075, x1z_075, TRAVEL_TIME, pivot_foot)
-        self.move_to(x1z_075, x1z_1, TRAVEL_TIME, pivot_foot)
+        # pivot_foot = 1
+        # self.latch_detach(pivot_foot)
+        # activate_servo(self.servo2)
+        # self.move_to(x1z1, x1z1_5, TRAVEL_TIME, pivot_foot)
+        # self.move_to(x1z1_5, x2z1_5, TRAVEL_TIME, pivot_foot)
+        # self.move_to(x2z1_5, x2z1, TRAVEL_TIME, pivot_foot)
+        # pivot_foot = 5
+        # self.latch_detach(pivot_foot)
+        # self.move_to(x2z_1, x2z_075, TRAVEL_TIME, pivot_foot)
+        # self.move_to(x2z_075, x1z_075, TRAVEL_TIME, pivot_foot)
+        # self.move_to(x1z_075, x1z_1, TRAVEL_TIME, pivot_foot)
 
 
         print("Movement complete: TEST_STEP")
