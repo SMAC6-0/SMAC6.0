b87d15f823cd69ab989bb03b6257c659b5b50e14
SakshiGauro <gaurosakshi113@gmail.com>
2025-04-24 20:01:53 -0400

    forward block stanky leg

--- inchworm_control/inchworm_control/ik_test.py
+++ inchworm_control/inchworm_control/ik_test.py
@@ -636,10 +636,15 @@ class IkTest(Node):
         # self.move_to(x1z0_5, x1z0, TRAVEL_TIME, pivot_foot)
 
         # move forward w/ blocks 
-        forward_block_time = 1
-        self.move_to(x2z_1, x2z_0_5, forward_block_time, pivot_foot)
-        self.move_to(x2z_0_5, x1z_0_5, forward_block_time, pivot_foot)
-        self.move_to(x1z_0_5, x1z_1, forward_block_time, pivot_foot)
+        block_travel_time = 1
+        # self.move_to(x2z_1, x2z_0_5, block_travel_time, pivot_foot)
+        # self.move_to(x2z_0_5, x1z_0_5, block_travel_time, pivot_foot)
+        # self.move_to(x1z_0_5, x1z_1, block_travel_time, pivot_foot)
+
+        # right turn w/ blocks
+        # self.move_to(y1z_1, y1z_0_5, TRAVEL_TIME, pivot_foot)
+        # self.move_to(y1z_0_5, x1z_0_5, TRAVEL_TIME, pivot_foot)
+        # self.move_to(x1z_0_5, x1z_1, TRAVEL_TIME, pivot_foot)
 
 
 
