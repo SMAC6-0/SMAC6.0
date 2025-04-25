5f921644ac853dc2a14ef0ad20b421db23b68da7
SakshiGauro <gaurosakshi113@gmail.com>
2025-04-24 20:16:30 -0400

    right block stanky leg

--- inchworm_control/inchworm_control/ik.py
+++ inchworm_control/inchworm_control/ik.py
@@ -281,9 +281,11 @@ def adjust_positions(goal_X: float, goal_Y: float, goal_Z: float, alpha: float, 
             
             elif goal_Y > 0.5: # y = 1 
                 if goal_Z > -1.1 and goal_Z <= -0.75: # z = -1
+                    inputX = CUBE_WIDTH * goal_X + 0
                     inputY = CUBE_WIDTH * goal_Y + 0
                     inputZ = CUBE_WIDTH * goal_Z + 0
                 elif goal_Z > -0.75 and goal_Z <= -0.25: # z = -0.5
+                    inputX = CUBE_WIDTH * goal_X + 0
                     inputY = CUBE_WIDTH * goal_Y + 0
                     inputZ = CUBE_WIDTH * goal_Z + 0
                 elif goal_Z > -0.25 and goal_Z <= 0.25: # z = 0
