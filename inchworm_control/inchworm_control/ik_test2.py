824bb5c07fab59483bddf3ff07896f2b17c4e61d
Rickwjx <rickwjx85@gmail.com>
2025-04-25 00:26:30 -0400

    Step forward block

--- inchworm_control/inchworm_control/ik_test2.py
+++ inchworm_control/inchworm_control/ik_test2.py
@@ -614,17 +614,17 @@ class IkTest2(Node):
         
 
         # Movements Pivot Foot 1
-        pivot_foot = 1
-        self.latch_detach(pivot_foot)
-        # Step Forward
-        # self.move_to(x1z0, x1z0_5, TRAVEL_TIME, pivot_foot)
-        # self.move_to(x1z0_5, x2z0_5, TRAVEL_TIME, pivot_foot)
-        # self.move_to(x2z0_5, x2z0, TRAVEL_TIME, pivot_foot)
+        # pivot_foot = 1
+        # self.latch_detach(pivot_foot)
+        # # Step Forward
+        # # self.move_to(x1z0, x1z0_5, TRAVEL_TIME, pivot_foot)
+        # # self.move_to(x1z0_5, x2z0_5, TRAVEL_TIME, pivot_foot)
+        # # self.move_to(x2z0_5, x2z0, TRAVEL_TIME, pivot_foot)
 
-        # Grab Block
-        self.move_to(x1z0, x1z1_5, TRAVEL_TIME, pivot_foot)
-        self.move_to(x1z1_5, x2z1_5, TRAVEL_TIME, pivot_foot)
-        self.move_to(x2z1_5, x2z1, TRAVEL_TIME, pivot_foot)
+        # # Grab Block
+        # self.move_to(x1z0, x1z1_5, TRAVEL_TIME, pivot_foot)
+        # self.move_to(x1z1_5, x2z1_5, TRAVEL_TIME, pivot_foot)
+        # self.move_to(x2z1_5, x2z1, TRAVEL_TIME, pivot_foot)
         # activate_servo(self.servo2)
 
         # Step Forward With Block
@@ -721,12 +721,12 @@ class IkTest2(Node):
         # self.move_to(x1z0_5, x1z0, TRAVEL_TIME, pivot_foot)
 
         # Step Forward Block
-        # pivot_foot = 1
-        # self.latch_detach(pivot_foot)
-        # activate_servo(self.servo2)
-        # self.move_to(x1z1, x1z1_5, TRAVEL_TIME, pivot_foot)
-        # self.move_to(x1z1_5, x2z1_5, TRAVEL_TIME, pivot_foot)
-        # self.move_to(x2z1_5, x2z1, TRAVEL_TIME, pivot_foot)
+        pivot_foot = 1
+        self.latch_detach(pivot_foot)
+        activate_servo(self.servo2)
+        self.move_to(x1z1, x1z1_5, TRAVEL_TIME, pivot_foot)
+        self.move_to(x1z1_5, x2z1_5, TRAVEL_TIME, pivot_foot)
+        self.move_to(x2z1_5, x2z1, TRAVEL_TIME, pivot_foot)
         pivot_foot = 5
         self.latch_detach(pivot_foot)
         self.move_to(x2z_1, x2z_075, TRAVEL_TIME, pivot_foot)
