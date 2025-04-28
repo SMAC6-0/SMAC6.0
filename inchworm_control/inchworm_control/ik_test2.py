6bcd3d1ea442db68e4a5daf467552e01bef00b3b
Rickwjx <rickwjx85@gmail.com>
2025-04-28 15:06:49 -0400

    Slowing down lift

--- inchworm_control/inchworm_control/ik_test2.py
+++ inchworm_control/inchworm_control/ik_test2.py
@@ -788,7 +788,7 @@ class IkTest2(Node):
             # Grab onto block again
         pivot_foot = 1
         self.latch_detach(pivot_foot)
-        self.move_to(x1z0, x1z1_5, TRAVEL_TIME, pivot_foot)
+        self.move_to(x1z0, x1z1_5, TRAVEL_TIME + 1, pivot_foot)
         self.move_to(x1z1_5, x2z1_5, TRAVEL_TIME, pivot_foot)
         self.move_to(x2z1_5, x2z1, TRAVEL_TIME, pivot_foot)
         activate_servo(self.servo2)
