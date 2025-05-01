2102b56deb8e7887e74d68c31b71cca748d2461d
tracy <tracy3846@gmail.com>
2025-04-30 22:24:09 -0400

    bleh

--- inchworm_control/block_simulation/config.py
+++ inchworm_control/block_simulation/config.py
@@ -2,15 +2,15 @@ from enum import Enum
 
 # this file holds variables that the user can change just in this file before running
 
-GRID_SIZE = 30  # Define the size of your grid in one direction 
+GRID_SIZE = 8  # Define the size of your grid in one direction 
                 # Then, use this dimension to create a 3D list representing the cubical workspace 
                 # where every cell is 0, representing that all those cells are walkable 
 GRID_HEIGHT = 4
 
 ### BLOCK DEPOTS & SEED BLOCK ###
-BD_1_LOC = [2, 3, 1] # The location where new blocks are sourced/placed to then be picked up by the inchworm robot, where the coordinates are represented as (x, y, z).
+BD_1_LOC = [4, 5, 1] # The location where new blocks are sourced/placed to then be picked up by the inchworm robot, where the coordinates are represented as (x, y, z).
 BD_LOCS = [BD_1_LOC] # The locations of all block depots (if there are multiple)
-SEED_BK = [6, 6, 1]
+SEED_BK = [2, 2, 1]
 
 ### INCHWORMS ###
 # - LOCATIONS - #
